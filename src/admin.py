8dbc59e207f6911404520bd048173f1189769672
rootavish <rootavish@gmail.com>
2014-11-12 13:48:48 +0530

    Almost finished Admin pages

--- src/admin.py
+++ src/admin.py
@@ -21,13 +21,21 @@ def getComplaintData():
 		data['type'] = complaint[1]
 		data['description'] = complaint[2]
 		data['action'] = complaint[3]
-		data['complaintee'] = complaint[4]
+		print complaint
+		g.database.execute("SELECT Username FROM entries WHERE User_id=%s" % (complaint[5]))
+		data['complainteeid'] = complaint[5]
+		data['complaintee'] = g.database.fetchone()[0]
+		g.database.execute("SELECT User_id FROM comments WHERE Comment_id=%s" % (complaint[4]))
+		data['defendantid'] = g.database.fetchone()[0]
 
+		g.database.execute("SELECT Username FROM entries WHERE User_id=%s" % (data['defendantid']))
+		data['defendantname'] = g.database.fetchone()[0]
 		retval.append(data)
 
 	return retval
 
-
 @admin.route('/admin/<userid>')
 def adminPage(userid):
-	return render_template('adminpage/index.html',complaints=getComplaintData(), form6 = searchForm(prefix='form6'))
+	return render_template('adminpage/index.html',
+							complaints=getComplaintData(), 
+							form6 = searchForm(prefix='form6'))
